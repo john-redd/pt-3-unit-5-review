d0cbb729922f8ef2a5879edc32d97dde3ac47988
John Redd <john.redd@endevvr.io>
2022-04-16 09:16:20 -0600

    Add fix for commas in client

--- client/index.js
+++ client/index.js
@@ -5,23 +5,23 @@ getPosts();
 async function getPosts() {
   const { data } = await axios.get(`${API_BASE_URL}/api/posts`);
 
-  console.log(data);
-
   addPostsToDOM(data);
 }
 
 function addPostsToDOM(posts) {
   const postsContainer = document.getElementById('posts-container');
 
-  const postElements = posts.map((p) => {
-    return `
+  const postElements = posts
+    .map((p) => {
+      return `
       <div>
         <h2>${p.title}</h2>
         <p>${p.content}</p>
         <p>By: ${p.user.firstName} ${p.user.lastName}</p>
       </div>
     `;
-  });
+    })
+    .join('');
 
   postsContainer.innerHTML = postElements;
 }
